8bd092cf68aba49de8253b5800c39fc3766f3edb
Marques <tiagobm564@gmail.com>
2020-09-09 21:35:26 -0300

    Added initial files

---
+++ teste-software-demo-1/src/main/java/com/javatech/testesoftwaredemo1/TesteSoftwareDemo1Application.java
@@ -0,0 +1,13 @@
+package com.javatech.testesoftwaredemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSoftwareDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSoftwareDemo1Application.class, args);
+	}
+
+}
---
+++ teste-software-demo-1/src/test/java/com/javatech/testesoftwaredemo1/TesteSoftwareDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatech.testesoftwaredemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSoftwareDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
